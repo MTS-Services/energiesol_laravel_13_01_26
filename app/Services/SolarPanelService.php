7ca3ef0655b9adb92de99d266921db27c00cd17b
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-01-17 16:05:42 +0600

    updated

--- app/Services/SolarPanelService.php
+++ app/Services/SolarPanelService.php
@@ -17,6 +17,24 @@ class SolarPanelService
     public function getQuery()
     {
         return $this->model->query();
+
+    }
+    public function create(array $data): SolarPanel
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $solarPanelId, array $data): SolarPanel|bool
+    {
         
+        $solarPanel = $this->model->findOrFail($solarPanelId);
+
+        if(!$solarPanel) {
+            return false;
+        }
+
+        $solarPanel->update($data);
+        
+        return $solarPanel;
     }
 }
