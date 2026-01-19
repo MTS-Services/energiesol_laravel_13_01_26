69d720bd1d475d79137e6a616fc7f19838e86f4e
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-01-19 17:49:55 +0600

    updated

--- resources/js/layouts/include/home/generation.tsx
+++ resources/js/layouts/include/home/generation.tsx
@@ -14,8 +14,8 @@ export default function generation() {
                     backgroundRepeat: 'no-repeat',
                 }}
             ></div>
-            <div className="relative z-10 mx-auto max-w-7xl px-6 py-15 lg:px-0 lg:py-30">
-                <div className='w-1/2'>
+            <div className="relative z-10 mx-auto max-w-7xl px-6 py-15 lg:px-0 lg:py-30 flex flex-row gap-5">
+                <div className='w-1/2 bg-linear-to-r from-btn-primary/15 to-info/5 p-6 rounded-xl'>
                     <div className="mb-3 flex flex-row items-center justify-start gap-3">
                         <span className='after:content[" "] relative inline-block h-5 w-5 rounded-[3px] bg-btn-primary after:absolute after:top-1 after:left-1 after:h-5 after:w-5 after:rounded-[3px] after:bg-info'></span>
                         <p className="mt-1 text-sm font-normal uppercase md:text-base md:text-secondary">
@@ -24,11 +24,13 @@ export default function generation() {
                     </div>
                     <div className="">
                         <h2 className="font-montserrat text-3xl font-semibold lg:text-[40px]">
-                            For
+                            For { ' '}
                             <span className="font-playfair-display font-semibold text-info italic">
-                              our generation
+                              our generation  {' '}
                             </span>
-                            and the ones after us
+                           <p>
+                             and the ones after us
+                           </p>
                            
                         </h2>
                     </div>
@@ -43,6 +45,24 @@ export default function generation() {
                             <Icon iconNode={ArrowRight} variant="circle"  ></Icon>
                             Configurator
                         </Button>
+                    </div>
+                </div>
+                <div className='grid grid-cols-2 gap-6 w-1/2'>
+                    <div className='bg-linear-to-r from-btn-primary/15 to-info/5 p-6 rounded-xl'>
+                        <h2 className='text3xl  lg:text-[40px] font-montserrat font-semibold text-info'>100%</h2>
+                        <p className='font-open-sans text-base text-secondary/50 lg:text-lg'>Clean Electricity</p>
+                    </div>
+                    <div className='bg-linear-to-r from-btn-primary/15 to-info/5 p-6 rounded-xl'>
+                        <h2 className='text3xl  lg:text-[40px] font-montserrat font-semibold text-info'>100%</h2>
+                        <p className='font-open-sans text-base text-secondary/50 lg:text-lg'>Clean Electricity</p>
+                    </div>
+                    <div className='bg-linear-to-r from-btn-primary/15 to-info/5 p-6 rounded-xl'>
+                        <h2 className='text3xl  lg:text-[40px] font-montserrat font-semibold text-info'>100%</h2>
+                        <p className='font-open-sans text-base text-secondary/50 lg:text-lg'>Clean Electricity</p>
+                    </div>
+                    <div className='bg-linear-to-r from-btn-primary/15 to-info/5 p-6 rounded-xl'>
+                        <h2 className='text3xl  lg:text-[40px] font-montserrat font-semibold text-info'>100%</h2>
+                        <p className='font-open-sans text-base text-secondary/50 lg:text-lg'>Clean Electricity</p>
                     </div>
                 </div>
             </div>
