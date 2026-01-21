7b29f0c621e639e88c3df45f14a6814f5c87c63c
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-01-21 16:29:56 +0600

    update

--- resources/js/components/cards/configurator-card.tsx
+++ resources/js/components/cards/configurator-card.tsx
@@ -1,34 +1,31 @@
 import { ArrowRight } from "lucide-react"
-import { Button } from "../ui/button"
-import { Icon } from "../ui/icon"
-import { Input } from "../ui/input"
-interface Porps {
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+interface Props {
    item?: {
-     changeOrder?: boolean|null
+     changeOrder?: boolean | null
    }
 }
 
-  function ConfiguratorCard({item}: Porps) {
+function ConfiguratorCard({item}: Props) {
   return (
-        <div className='grid lg:grid-cols-2 gird-cols gap-6 lg:gap-9'>
+        <div className='grid lg:grid-cols-2 grid-cols-1 gap-6 lg:gap-9'>
 
             <div className={`py-20 rounded-xl ${item?.changeOrder ? 'lg:order-last' : ''}`}>
                 <div className="mb-3 flex flex-row items-center justify-start gap-3">
-                    <span className='after:content[" "] relative inline-block h-5 w-5 rounded-[3px] bg-btn-primary after:absolute after:top-1 after:left-1 after:h-5 after:w-5 after:rounded-[3px] after:bg-info'></span>
+                    <span className='after:content-[""] relative inline-block h-5 w-5 rounded-[3px] bg-btn-primary after:absolute after:top-1 after:left-1 after:h-5 after:w-5 after:rounded-[3px] after:bg-info'></span>
                     <p className="mt-1 text-sm font-normal uppercase md:text-base md:text-secondary">
                         3 Min Setup
                     </p>
                 </div>
                 <div className="">
                     <h2 className="font-montserrat text-3xl font-semibold lg:text-[40px]">
-                        PV system in  { ' '}
+                        PV system in{' '}
                         <span className="font-playfair-display font-semibold text-info italic">
-                        only 3 minutes  {' '}
+                        only 3 minutes{' '}
                         </span>
-
                         configured
-
-                    
                     </h2>
                 </div>
                 <div className="flex flex-col">
@@ -39,18 +36,18 @@ interface Porps {
 
                 <div className="mt-10 grid grid-cols-2 gap-4 items-center overflow-hidden">
                     <div>
-                        <Input type='number' className='rounded-full shadow-[inset_1px_1px_20px_3px_#fff] px-4! py-6!' placeholder='80'/>
+                        <Input type='number' className='rounded-full shadow-[inset_1px_1px_20px_3px_#fff] px-4 py-6' placeholder='80'/>
                     </div>
                     <div>
-                        <Button className=' text-sm lg:text-base' variant={'default'}>
-                        <Icon iconNode={ArrowRight} variant="circle"  ></Icon>
+                        <Button className='text-sm lg:text-base' variant={'default'}>
+                        <ArrowRight className="mr-2 h-4 w-4" />
                         Start Configurator
                     </Button>
                     </div>
                 </div>
             </div>
 
-            <div className={`hidden lg:grid grid-cols gap-6 py-8.5 ${item?.changeOrder ? 'lg:order-fast' : ''}`}>
+            <div className={`hidden lg:grid grid-cols-1 gap-6 py-8.5 ${item?.changeOrder ? 'lg:order-first' : ''}`}>
                 <div className='w-full h-full rounded-2xl'
                 style={{
                     backgroundImage: "url('images/photo-group.png')",
@@ -58,9 +55,7 @@ interface Porps {
                     backgroundPosition: 'center center',
                 }}
                 >
-
                 </div>
-
             </div>
         </div>
   )
