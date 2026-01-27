117fd6b817728c99960b890aa13750a7f1a9f6c9
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-01-27 09:05:53 +0600

    update

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,10 @@
-import { Link } from '@inertiajs/react';
 
+import { cn } from '@/lib/utils';
 
-export default function AppLogo() {
+export default function AppLogo({className  }: {className?: string}) {
     return (
-        <div className="flex items-center gap-2.5">
-       <p className='font-montserrat font-semibold text-lg lg:text-[28px] text-secondary'> <span className='text-info'>E</span>nergie <span className='text-info'>S</span>olution <span className='text-info'>N</span>ord</p>
+        <div className={`flex items-center gap-2.5 ${className}`}>
+       <p className={cn('font-montserrat font-semibold text-lg lg:text-[28px] text-secondary',className)}> <span className='text-info'>E</span>nergie <span className='text-info'>S</span>olution <span className='text-info'>N</span>ord</p>
         </div>
     );
 }
