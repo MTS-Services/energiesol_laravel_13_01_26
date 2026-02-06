011abc9f061cc92b31ad83f6c3777f2d0fda7d68
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-02-06 16:03:17 +0600

    updated

--- resources/js/components/sections/order/cart-details.tsx
+++ resources/js/components/sections/order/cart-details.tsx
@@ -377,7 +377,7 @@ function handlePdfDownload(e) {
                     </div>
 
                     <div className="flex items-center justify-center pt-21">
-                        <Link className="inline-flex items-center gap-3 text-secondary/70">
+                        <Link href={route('configurator')} className="inline-flex items-center gap-3 text-secondary/70">
                             <Icon
                                 iconNode={RefreshCcw}
                                 iconClassName="text-secondary/70 w-4 h-4"
