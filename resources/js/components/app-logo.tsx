e838e8e161fbfb72d78ecc85d65cd22b819142e8
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-02-17 16:03:07 +0600

    update

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,7 +4,8 @@ import { cn } from '@/lib/utils';
 export default function AppLogo({className  }: {className?: string}) {
     return (
         <div className={`flex items-center gap-2.5 ${className}`}>
-       <p className={cn('font-montserrat font-semibold text-lg lg:text-[28px] text-secondary',className)}> <span className='text-info'>E</span>nergie <span className='text-info'>S</span>olution <span className='text-info'>N</span>ord</p>
+            <img src="/images/logo-2.png" alt="" className='w-30'/>
+       {/* <p className={cn('font-montserrat font-semibold text-lg lg:text-[28px] text-secondary',className)}> <span className='text-info'>E</span>nergie <span className='text-info'>S</span>olution <span className='text-info'>N</span>ord</p> */}
         </div>
     );
 }
